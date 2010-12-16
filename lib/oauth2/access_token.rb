68f386cf42ee4f84c19b4a405b11dc92d2f133d6
Trotter Cashion <cashion@gmail.com>
2010-12-16 14:16:46 -0500

    Use Authorization header that Salesforce expects

--- lib/oauth2/access_token.rb
+++ lib/oauth2/access_token.rb
@@ -17,7 +17,8 @@ module OAuth2
     end
 
     def request(verb, path, params = {}, headers = {})
-      @client.request(verb, path, params.merge('access_token' => @token), headers.merge('Authorization' => "Token token=\"#{@token}\""))
+      @client.request(verb, path, params,
+                      headers.merge('Authorization' => "OAuth #{@token}"))
     end
 
     def get(path, params = {}, headers = {})
